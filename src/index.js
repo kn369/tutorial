bd3de5f5fbb63b93e0650e4c7d5760f968257ad6
Krish Nariya <knariya0369@gmail.com>
2024-10-25 18:42:51 +0530

    Created Repository

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"; // This is ES6 functionality. It is not specific to react.
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function BookList() {
+	return (
+		<section className="bookList">
+			<Book />
+		</section>
+	);
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+	return (
+		<article className="book">
+         <img
+            src="https://images-na.ssl-images-amazon.com/images/I/81eB+7+CkUL.jpg"
+            alt=""
+            width="200px"
+         />
+         <h1>I love you to the moon</h1>
+         <h4 style={{ color: "#617", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+            Amelia Hepworth
+         </h4>
+		</article>
+	);
+};
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
+
+// function Greeting() {
+// 	return (
+//       <div>
+//          <Person />
+//          <Message />
+//       </div>
+// 	);
+// }
+
+// const Person = () => {
+//    return <h2>John Doe</h2>;
+// }
+
+// const Message = () => {
+//    return <p>this is my message</p>;
+// }
+// const Greeting = () => {
+// 	return React.createElement(
+// 		"div",
+// 		{},
+// 		React.createElement("h1", {}, "Hello World")
+// 	);
+// };
